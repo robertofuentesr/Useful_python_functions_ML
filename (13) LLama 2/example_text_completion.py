e4cfa8740f954ce64b0165180dbb3f884dd0315e
Roberto Fuentes <rrisco1990@gmail.com>
2025-08-30 13:51:42 -0400

    cleaning code this black

--- (13) LLama 2/example_text_completion.py
+++ (13) LLama 2/example_text_completion.py
@@ -8,14 +8,15 @@ from llama import Llama
 # import torch.distributed as dist
 # dist.init_process_group(backend='gloo', rank=0, world_size=4, init_method='file:///tmp/sharedfile')
 
-import os 
+import os
 import torch
+
 #  read my comment here in case you are having problems :)
 #  https://github.com/meta-llama/llama/issues/699#issuecomment-1688777017
 
-#os.environ['PL_TORCH_DISTRIBUTED_BACKEND'] = 'gloo'
-#os.environ['NCCL_DEBUG'] = 'INFO'
-#torch.distributed.init_process_group(backend="gloo")
+# os.environ['PL_TORCH_DISTRIBUTED_BACKEND'] = 'gloo'
+# os.environ['NCCL_DEBUG'] = 'INFO'
+# torch.distributed.init_process_group(backend="gloo")
 
 
 def main(
@@ -54,17 +55,18 @@ def main(
         temperature=temperature,
         top_p=top_p,
     )
-    list_text =[]
+    list_text = []
     for prompt, result in zip(prompts, results):
         print(prompt)
         print(f"> {result['generation']}")
         print("\n==================================\n")
         list_text.append(prompt)
-        list_text.append(result['generation'])
+        list_text.append(result["generation"])
         list_text.append("\n==================================\n")
-    text = ''.join(list_text)
+    text = "".join(list_text)
     with open("output.txt", "w") as file:
         file.write(text)
+
 
 if __name__ == "__main__":
     fire.Fire(main)
